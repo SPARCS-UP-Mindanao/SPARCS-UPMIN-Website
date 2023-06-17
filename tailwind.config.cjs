8942a20ce13643a6dc7c63c109462455615725e0
ArJSarmiento <rneljan@gmail.com>
2023-06-17 23:55:23 +0800

    HOTFIX tailwind setup and component breakdown

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				raleway: ['Raleway', 'sans-serif'],
+				montserrat: ['Montserrat', 'sans-serif'],
+			},
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				dazzlingBlue: '#3853A4',
+			}
+		},
 	},
 	plugins: [],
 }
