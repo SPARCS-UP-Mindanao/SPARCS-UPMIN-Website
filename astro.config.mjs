ed9c4b534408bc7e75ffdd8f827073a2c1cf910e
ArJSarmiento <rneljan@gmail.com>
2023-06-18 12:51:55 +0800

    astro base url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://arjsarmiento.github.io',
+  base: '/SPARCS-UPMIN-Website',
   integrations: [tailwind(), react()]
 });
