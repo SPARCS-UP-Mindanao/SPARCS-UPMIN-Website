5738bf90c0c7d0f995542635ae624e6361829a0d
seangaaab <seangabbayron@gmail.com>
2023-06-18 10:33:17 +0800

    added arca font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				raleway: ['Raleway', 'sans-serif'],
-				montserrat: ['Montserrat', 'sans-serif'],
-			},
-			colors: {
-				transparent: 'transparent',
-				current: 'currentColor',
-				dazzlingBlue: '#3853A4',
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+        arca: ["ArcaMajora3", "sans-serif"],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        dazzlingBlue: "#3853A4",
+      },
+    },
+  },
+  plugins: [],
+};
