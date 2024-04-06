7ee574acacb5c1b7b9228de725a75cf34fdb21b9
Mojhee <aneureza@gmail.com>
2024-04-06 11:48:01 +0800

    feat: change SPARCS Logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import logo from "/logo.png";
+import logo_word from "/logo_word.png";
 import { useState, useEffect } from 'react';
 
 function Header() {
@@ -7,16 +8,21 @@ function Header() {
     setScroll(window.scrollY > 30);
   }
 
+  const width = window.innerWidth;
+  
   useEffect(() => {
     window.addEventListener("scroll", scrollHandler, true);
   }, []);
+
+  const sparcs_word = <img src={logo_word} alt="SPARCS" className="h-16" />;
+  const sparcs_s = <img src={logo} alt="SPARCS" className="w-16" />;
 
   return (
     <header
       className={`fixed top-0 w-full flex flex-row items-center justify-around md:py-5 py-3 z-40 ${scroll ? 'blurNav' : ''}`}
     >
       <a href="#sparcs">
-        <img src={logo} alt="SPARCS" className="w-16" />
+        {width > 768 ? sparcs_word : sparcs_s}
       </a>
       <nav className="flex flex-row md:gap-10 gap-3 z-50">
         <a href="#about" className="text-white font-montserrat md:text-lg group transition duration-300">
